9882725634d5cc12b496fe71017dfbd22e028b52
aXXo-dev <hunters12@protonmail.com>
2023-10-07 14:12:11 -0400

    Fixed API not working in container

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,7 @@ import * as V1Data from "./routes/v1/data";
 import * as V1Flag from "./routes/v1/flag";
 
 try {
+  const host = "0.0.0.0";
   const port = 8080;
 
   const server = fastify();
@@ -15,9 +16,9 @@ try {
   server.register(V1Data.data);
   server.register(V1Flag.flag);
 
-  server.listen({ port: port });
+  server.listen({ host: host, port: port });
 
-  console.log("[+] API up on port " + port);
+  console.log(`[+] API up on ${host} port ${port}`);
 } catch (e) {
   console.log(e);
 }
